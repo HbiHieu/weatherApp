139ce2b054c9f4c6c7de2828126fa12fa991f8a7
HbiHieu <89714916+HkHieus@users.noreply.github.com>
2022-06-08 01:32:53 +0700

    remove rainy animation

--- test.js
+++ test.js
@@ -103,17 +103,6 @@ submitBtn.addEventListener('click',function(){
             }
         }
         else {
-            let hrElement;
-  let counter = 100;
-  for (let i = 0; i < counter; i++) {
-    hrElement = document.createElement("HR");
-    
-      hrElement.style.left = Math.floor(Math.random() * window.innerWidth) + "px";
-      hrElement.style.animationDuration = 3 + Math.random() * 0.3 + "s";
-      hrElement.style.animationDelay = Math.random() * 5 + "s";
-   
-    document.body.appendChild(hrElement);
-  }
             if( (hours>= 19) || (hours <=6) ) {
                 weatherIcon.innerHTML = `<i class="fa-solid fa-cloud-moon-rain"></i>`;
             }
